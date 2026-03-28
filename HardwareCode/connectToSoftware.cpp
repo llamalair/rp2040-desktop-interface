b0c5e56b8bd9266c8a3d1df07076307262cfb544
LouisMartin2005 <pnglouismartin@gmail.com>
2026-03-28 16:46:15 +0000

    syntax error

--- HardwareCode/connectToSoftware.cpp
+++ HardwareCode/connectToSoftware.cpp
@@ -19,11 +19,11 @@ int main()
 
         if (user_input == '1' ){
             pico_set_led(true);
-            printf("ON\n")
+            printf("ON\n");
         }
         else if (user_input == '0'){
             pico_set_led(false);
-            printf("OFF\n")
+            printf("OFF\n");
         }
     }
 }
